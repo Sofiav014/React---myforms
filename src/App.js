7e111b13a3ea25b64109ace9b22fdc88631be514
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2023-09-22 08:56:43 -0500

    Ejercicio Clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
+  const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"})
+  const [emailValid, setEmailValid] = useState(false); // Track email field validation
+  const [passwordValid, setPasswordValid] = useState(false); // Track password field validation
+  const [touched, setTouched] = useState({ email: false, password: false });
+
+
+
+  const handleEmailChange = ((e) => {
+    const email = e.target.value;
+    setFormValues({ ...formValues, email });
+    
+    // Check if the email is not null (empty or undefined)
+
+  });
+ 
+  const handlePasswordChange = ((e) => {
+    const password = e.target.value;
+    setFormValues({ ...formValues, password });
+    setTouched({ ...touched, password: true });
+    // Check if the password is not null (empty or undefined)
+    setPasswordValid(password.trim() !== '' && password.length >= 9 && password.match(/[a-z]/i) && password.match(/[0-9]/));
+
+  });
+ 
+  const handleSelectChange = ((e) => {
+    setFormValues({...formValues, favClass: e.target.value})
+  });
+
+  const clickSubmit = () => {
+    setTouched({ ...touched, email: true });
+    const REGEX = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/
+    // Check if the email is not null (empty or undefined)
+    const valid = REGEX.test(formValues.email)
+
+    setEmailValid(valid);
+
+    // Check if both email and password are not null
+    if (valid && passwordValid) {
+      // Perform form submission
+      alert(JSON.stringify(formValues));
+    } else {
+      alert('Please fill in all required fields.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ejemplo de formularios!</h1>
+     
+      <Form>
+      <Form.Group className="mb-6" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email} isInvalid={touched.email && !emailValid} isValid={touched.email && emailValid}/>
+        { !emailValid && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
+
+        {/* <Form.Control.Feedback type="invalid">Please enter a valid email.</Form.Control.Feedback> */}
+      </Form.Group>
+ 
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} isInvalid={touched.password && !passwordValid} isValid={touched.password && passwordValid} />
+        { !passwordValid && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
+
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Label>Favorite Class</Form.Label>
+        <Form.Select onChange={handleSelectChange}>
+          <option value="1">ISIS3710</option>
+          <option value="2">Programación con tecnologias web</option>
+        </Form.Select>
+      </Form.Group>
+      <Button variant="primary" onClick={clickSubmit}>
+        Submit
+      </Button>
+    </Form>
+    
     </div>
   );
 }
